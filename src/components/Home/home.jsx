ce8fe3d37b18426ce20feb245c660c06299a922a
SathyajithNairT <sathyajithnairwork@gmail.com>
2024-07-07 14:02:57 +0530

    Changed from Browser Reouter to HashRouter as the pervious depyoyed page was blank

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,5 +1,5 @@
 import './home.css'
-import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import { HashRouter, Router, Route, Routes } from 'react-router-dom'
 import Sign_In from '../sign_in_page/sign-in'
 import User_Page from '../user_page/user-page'
 
@@ -7,12 +7,12 @@ import User_Page from '../user_page/user-page'
 function Home() {
     return (
         <div className='home-wrap d-flex flex-column'>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path='/' element={<Sign_In />}/>
                     <Route path='/user' element={<User_Page />}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     )
 }
